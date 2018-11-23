4b0a196093d275545a78ca208c7c59aa71f2793e
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-11-23 17:05:58 +0300

    add page Promote add section First add section Price

--- js/main.js
+++ js/main.js
@@ -245,29 +245,33 @@
         });
     }
 
-    let costSwithes = document.querySelector('.site-cost__switches-list'),
-        costSwithesWidth = costSwithes.offsetWidth,
-        costSwithesList = document.querySelectorAll('.site-cost__switch'),
-        costLine = document.querySelector('.site-cost__subline'),
-        costTabs = document.querySelectorAll('.site-cost__tab');
 
-    for (let i = 0; i < costSwithesList.length; i++) {
-        costSwithesList[i].addEventListener('click', function (e) {
-            let percent = (this.offsetLeft / costSwithesWidth) * 100,
-                id = this.dataset.id,
-                tab = document.querySelector('.site-cost__tab[data-id="' + id + '"]');
+    let costSwithes = document.querySelector('.site-cost__switches-list');
 
-            for (let i = 0; i < costSwithesList.length; i++) {
-                costSwithesList[i].classList.remove('active');
-            }
-            for (let i = 0; i < costTabs.length; i++)  {
-                costTabs[i].classList.remove('active');
-            }
-            tab.classList.add('active');
-            this.classList.add('active');
-            costLine.style.left = percent + '%';
+    if (costSwithes) {
+        let costSwithesWidth = costSwithes.offsetWidth,
+            costSwithesList = document.querySelectorAll('.site-cost__switch'),
+            costLine = document.querySelector('.site-cost__subline'),
+            costTabs = document.querySelectorAll('.site-cost__tab');
 
-        });
+        for (let i = 0; i < costSwithesList.length; i++) {
+            costSwithesList[i].addEventListener('click', function (e) {
+                let percent = (this.offsetLeft / costSwithesWidth) * 100,
+                    id = this.dataset.id,
+                    tab = document.querySelector('.site-cost__tab[data-id="' + id + '"]');
+
+                for (let i = 0; i < costSwithesList.length; i++) {
+                    costSwithesList[i].classList.remove('active');
+                }
+                for (let i = 0; i < costTabs.length; i++) {
+                    costTabs[i].classList.remove('active');
+                }
+                tab.classList.add('active');
+                this.classList.add('active');
+                costLine.style.left = percent + '%';
+
+            });
+        }
     }
 
 

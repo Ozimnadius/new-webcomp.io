6ce509c9e447c85c6339e4387d7cff46850ffc6b
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-12-05 15:19:28 +0300

    add section Faq

--- js/pages/seo.js
+++ js/pages/seo.js
@@ -10,4 +10,35 @@
             autoWidth: true
         });
     }
+
+    let acco = document.querySelector('.seo-faq__acco'),
+        accoItems = document.querySelectorAll('.seo-faq__item');
+
+    if (acco) {
+        acco.addEventListener('click', function (e) {
+            let target = e.target;
+
+            if (target.classList.contains('seo-faq__item-title')) {
+
+                let item = target.parentNode,
+                    itemWrap = item.querySelector('.seo-faq__item-wrap'),
+                    itemContent = item.querySelector('.seo-faq__item-desc'),
+                    height = itemContent.offsetHeight+'px';
+
+
+                for (let i = 0; i < accoItems.length; i++) {
+                    if (accoItems[i] != item) {
+                        accoItems[i].querySelector('.seo-faq__item-wrap').style.height = '';
+                    }
+                }
+
+                if (!itemWrap.style.height) {
+                    itemWrap.style.height = height;
+                } else {
+                    itemWrap.style.height = '';
+                }
+            }
+
+        });
+    }
 }());
